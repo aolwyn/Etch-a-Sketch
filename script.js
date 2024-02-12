1b010c38cc60b4d7a1b2f11a010ad8975319c01e
Adam Bayley <adambayley2@gmail.com>
2024-02-12 17:41:17 -0500

    work on grid gen

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const clear = document.querySelector('#clear')
 
 clear.addEventListener('click', () => {
     // alert("cleared grid!");
-    document.querySelector('.grid').textContent = '';
-    
+    cleargrid();
 })
+
 
 submit.addEventListener('click', () => {
     // alert(size.value);
@@ -17,12 +17,27 @@ submit.addEventListener('click', () => {
 
 let color = "black";
 
+function cleargrid(){
+    document.querySelector('.grid').textContent = '';
+}
+
 function buildGrid(size){
     let gridSize = 0;
     if(size <16 || size > 100 || isNaN(size))
         gridSize = 16;
     else{
         gridSize = size;
+    }
+
+    //the idea here will to be create a row for block, then add columns to it.
+    for(let i = 0; i < size; i++){
+        let lines = document.createElement('div');
+        lines.classList.add('line')
+        document.querySelector('.grid').appendChild(lines);
+        for(let j = 0; j < size; j++){
+
+        }
+
     }
 
 
