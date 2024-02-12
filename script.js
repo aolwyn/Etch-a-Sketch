c333a940a971e9b3aa6d25fc588c5a6fd55f04f7
Adam Bayley <adambayley2@gmail.com>
2024-02-12 17:17:47 -0500

    added clear grid button

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const submit = document.querySelector('#submit')
 const size = document.querySelector('#size')
 
+const clear = document.querySelector('#clear')
+
+clear.addEventListener('click', () => {
+    // alert("cleared grid!");
+    document.querySelector('.grid').textContent = '';
+    
+})
+
 submit.addEventListener('click', () => {
-    //alert(size.value)
+    // alert(size.value);
     let gridSize = size.value;
     buildGrid(gridSize);
 })
@@ -16,6 +24,6 @@ function buildGrid(size){
     else{
         gridSize = size;
     }
-    
+
 
 }
